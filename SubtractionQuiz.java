1ec8db3f87f076a0f4b86851c7bdca2e57d1ea31
glenjoo84 <glenjoo84@gmail.com>
2016-10-13 12:53:03 +1000

    Commenting SubtractionQuiz.java

--- SubtractionQuiz.java
+++ SubtractionQuiz.java
@@ -6,31 +6,31 @@
 import java.util.Scanner;
 
 public class SubtractionQuiz {
-    public static void main(String[] args) {
-	    
-      int number1 = (int)(Math.random() * 10); 
-	    int number2 = (int)(Math.random() * 10);
+  public static void main(String[] args) {
+    // assigning my own values to see the next if statement's property	    
+    int number1 = 88; // (int)(Math.random() * 10); 
+    int number2 = 44; //(int)(Math.random() * 10);
 
-      // here in the follow if { scope, I am observing how computer executes
-      // the following statements in the top to bottom order.
-      if (number1 < number2) {
-        int temp = number1;
-        number1 = number2;
-        number2 = temp;
-	    }
-
-	    System.out.print("What is " + number1 + " - " + number2 + "? ");
-	    Scanner input = new Scanner(System.in);
-	    
-      int answer = input.nextInt();
-
-	    if (number1 - number2 == answer)
-        System.out.println("You are correct!");
-	
-      else {
-        System.out.println("Your answer is wrong.");
-		    System.out.println(number1 + " - " + number2 + " should be " 
-                            + (number1 - number2));
-      }
+    // here in the follow if { scope, I am observing how computer executes
+    // the following statements in the top to bottom order.
+          
+    if (number1 < number2) {
+      int temp = number1; // temp is declared, and 88 is assigned to temp
+                          // thus, now  temp = 88;
+      number1 = number2;  // now, number1 = 44;
+      number2 = temp;     // now, number2 = 88;
     }
+
+    System.out.print("What is " + number1 + " - " + number2 + "? ");
+    Scanner input = new Scanner(System.in);
+	    
+    int answer = input.nextInt();
+    if (number1 - number2 == answer)
+      System.out.println("You are correct!");
+    else {
+      System.out.println("Your answer is wrong.");
+      System.out.println(number1 + " - " + number2 + " should be " 
+                            + (number1 - number2));
+    }
+  }
 }		
